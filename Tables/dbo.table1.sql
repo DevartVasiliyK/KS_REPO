cf9ae1f27219d8443228810cde440f881b139f0c
KsushaS <KsushaS@devart.com>
2015-07-30 12:22:02 +0300

    dd

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE dbo.table1 (
   aa int IDENTITY,
-  a varchar(50) NULL,
   b varchar(50) NULL,
+  c varchar(50) NULL,
   CONSTRAINT PK_table1 PRIMARY KEY CLUSTERED (aa)
 )
 ON [PRIMARY]
