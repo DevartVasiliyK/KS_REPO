453a89e491a98897e8adca435f8cff3cbc568e52
KsushaS <KsushaS@devart.com>
2015-11-06 14:19:44 +0200

    00

---
+++ Security/Roles/werwer.sql
@@ -0,0 +1,5 @@
+﻿CREATE ROLE [werwer] AUTHORIZATION [user1]
+GO
+
+EXEC sp_addrolemember N'werwer', N'user1'
+GO
---
+++ Security/Schemas/Sales.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [Sales] AUTHORIZATION [user1]
+GO
+
+EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to customers, sales orders, and sales territories.', 'SCHEMA', N'Sales'
+GO
