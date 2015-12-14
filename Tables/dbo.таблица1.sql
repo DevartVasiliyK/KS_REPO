5103afc523600f8f5b0d91e4ee39a81103e512da
KsushaS <KsushaS@devart.com>
2015-12-14 12:49:48 +0200

    del
    del

--- Tables/dbo.таблица1.sql
+++
@@ -1,8 +0,0 @@
-﻿
-
-CREATE TABLE dbo.таблица1 (
-  колонка1 int NULL,
-  колонка2 varchar(50) NULL,
-  колонка4 varchar(50) NULL
-)
-GO
--- Tables/dbo.таблица2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dbo.таблица2 (
-  колонка1 int NULL,
-  колонка2 varchar(50) NULL,
-  колонка4 varchar(50) NULL
-)
-GO
